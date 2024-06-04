7d367eef7333bf60e2d47acaab53e5bd36fc4b5c
nakasanje <joanug19@gmail.com>
2024-06-04 12:53:55 +0300

    models

--- cassavas/models.py
+++ cassavas/models.py
@@ -1,3 +1,6 @@
+# myapp/models.py
 from django.db import models
 
-# Create your models here.
+class CassavaImage(models.Model):
+    image = models.ImageField(upload_to='cassava/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
